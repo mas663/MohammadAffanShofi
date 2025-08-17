39d2d058c75877c27cb997e3272d96b1803b4814
Mohammad Affan Shofi <affan8971@gmail.com>
2025-08-17 15:55:43 +0700

    chore: change favicon to new logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mohammad Affan Shofi - Portfolio",
+  description: "Personal Portfolio Website",
+  icons: {
+    icon: { url: "/favicon.png", type: "image/png" },
+  },
+};
 
 export default function RootLayout({
   children,
